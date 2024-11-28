5072455673d7219691662a7f85b1ee6cd4f6792f
Szabina Korbai <skorbai@student.hive.fi>
2024-11-28 10:40:57 +0200

    Added configuration for setting up volumes correctly (yet to be tested)

--- srcs/requirements/tools/project_setup.sh
+++ srcs/requirements/tools/project_setup.sh
@@ -6,12 +6,14 @@ if test -d /home/skorbai/data/wordpress; then
 	echo "Wordpress volume already exists"
 else
 	echo "Wordpress volume not created yet. Making directory now"
-	#mkdir /home/skorbai/data/wordpress
+	mkdir /home/skorbai/data/wordpress
+	sudo chown -R 1000 /home/skorbai/data/wordpress
 fi
 
 if test -d /home/skorbai/data/database; then
 	echo "Database volume already exists"
 else
 	echo "Database volume not created yet. Making directory now"
-	#mkdir /home/skorbai/data/wordpress
+	mkdir /home/skorbai/data/wordpress
+	sudo chown -R 1000 /home/skorbai/data/wordpress
 fi
