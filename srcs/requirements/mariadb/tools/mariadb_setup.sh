abf3c4d12ef7cd090fe3a72921ebf0d280b66cf7
Szabina Korbai <skorbai@student.hive.fi>
2024-11-28 15:45:06 +0200

    mariadb_install_db command did not work, changed it to mysql_install_db (which it supposed to have a symlink to) Also changed mariadbd to mysqld

--- srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -7,9 +7,9 @@ echo "Initializing mariaDB data directory and creating system tables"
 if test -d "/var/lib/mysql/mysql"; then
 	echo "MariaDB already initialized"
 else
-	mariadb_install_db --datadir=/var/lib/mysql --user=mysql
+	mysql_install_db --datadir=/var/lib/mysql --user=mysql
 	echo "Initialization started. Creating database..."
-	mariadbd --user=mysql --bootstrap << EOF
+	mysqld --user=mysql --bootstrap << EOF
 FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
 CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
