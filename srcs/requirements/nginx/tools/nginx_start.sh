33bb89ebffc8a4ce90911b220f1d0fe8f79643b2
skorbai <skorbai@Inception.skorbai.42.fr>
2024-12-03 15:22:52 +0200

    Tried some things to fix ssl problem, next: refactoring to not have include directives

--- srcs/requirements/nginx/tools/nginx_start.sh
+++ srcs/requirements/nginx/tools/nginx_start.sh
@@ -11,8 +11,8 @@ else
 	openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $CERTS_KEY \
 	 -out $CERTS_CRT -subj "/CN=$DOMAIN_NAME"
 
-	echo "Generating dh parameters - this will take a while"
-	openssl dhparam -out /etc/nginx/dhparam.pem 4096
+	#echo "Generating dh parameters - this will take a while"
+	#openssl dhparam -out /etc/nginx/dhparam.pem 4096
 	echo "Certificate successfully generated"
 
 	echo "Adding certificate location info to self-signed.conf"
