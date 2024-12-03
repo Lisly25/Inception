9ee6c22ace28985c3eb6298d674c80a1e790aa5f
Szabina Korbai <skorbai@student.hive.fi>
2024-12-03 11:34:37 +0200

    Re-adding dh param creation, since now I can't go to the website because the site can't provide a secure connection

--- srcs/requirements/nginx/tools/nginx_start.sh
+++ srcs/requirements/nginx/tools/nginx_start.sh
@@ -11,8 +11,8 @@ else
 	openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $CERTS_KEY \
 	 -out $CERTS_CRT -subj "/CN=$DOMAIN_NAME"
 
-	#echo "Generating dh parameters - this will take a while"
-	#openssl dhparam -out /etc/nginx/dhparam.pem 4096
+	echo "Generating dh parameters - this will take a while"
+	openssl dhparam -out /etc/nginx/dhparam.pem 4096
 	echo "Certificate successfully generated"
 
 	echo "Adding certificate location info to self-signed.conf"
@@ -22,6 +22,7 @@ else
 
 fi
 
+echo "Nginx configuration done. Starting nginx"
 
 #Replaces current process with nginx (running in the foreground), becoming PID 1
 exec nginx -g 'daemon off;'
