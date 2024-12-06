b98fe9fc864670f6d4ad8012baf7be64230e9b72
skorbai <skorbai@Inception.skorbai.42.fr>
2024-12-06 12:57:14 +0200

    Changed database setup: now you need a password to access the database, even if you are logged in as root

--- srcs/requirements/mariadb/tools/mariadb_setup.sh
+++ srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -7,14 +7,14 @@ echo "Initializing mariaDB data directory and creating system tables"
 if test -d "/var/lib/mysql/mysql"; then
 	echo "MariaDB already initialized"
 else
-	mysql_install_db --datadir=/var/lib/mysql --user=mysql --skip-test-db --auth-root-authentication-method=socket
+	mysql_install_db --datadir=/var/lib/mysql --user=mysql --skip-test-db
 	echo "Initialization started. Creating database..."
 	mysqld --user=mysql --bootstrap << EOF
 FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
 CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
-GRANT ALL PRIVILEGES ON *.* to 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 FLUSH PRIVILEGES;
 EOF
 	echo "Database \" $MYSQL_DATABASE \" has been created successfully"
